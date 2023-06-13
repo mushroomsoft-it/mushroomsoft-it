883a481681994423bb2d7e401e3b13d5a7341d35
JamilOmar <JamilOmar@users.noreply.github.com>
2023-06-13 15:36:17 +0000

    Deploying to gh-pages from @ mushroomsoft-it/mushroomsoft-ws@de55fc8f650aba38cc01b902f08199a77b20e490 🚀

--- mushroomsoft/lib/src/lib/shared/constants/constants.ts
+++
@@ -1,1 +0,0 @@
-export const API_STRAPI = process.env['NG_APP_STRAPI_API_URL'] + 'forms?filters[type][$eq]=';
--- mushroomsoft/lib/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of lib
- */
-
-export * from './lib/services/lib.service';
-export * from './lib/lib.module';
