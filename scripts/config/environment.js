9f1dd88e91f7576b1486ab6fa3f5ddbc43f700b3
Juan Pablo Cortez Mosquera <juanpacortexz93@gmail.com>
2025-02-20 23:22:21 -0500

    update: new power automate flow url updated

--- scripts/config/environment.js
+++ scripts/config/environment.js
@@ -1,9 +1,9 @@
 const environmentConst = {
-  powerAutomateUri: "https://prod-01.brazilsouth.logic.azure.com:443/workflows/376a472c592841a6bc6ce35242fd5a65/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=k72tjaGBJ90_5KJvskxsPZEjwLAPA1Ii8RswhzvRqUY",
+  powerAutomateUri: "https://prod-23.brazilsouth.logic.azure.com:443/workflows/317ab7442f8944c1b3aa9fa78ea4b160/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=vhRZmEbyZvgKeyxZXENpOAd0magVOt6Owpr91V7e6vo",
   employeesProfilePhotoFolder: "../public/employeesProfilePhoto",
   publicFolder: "../public/",
   configAppProject: "../src/app/config/",
   dataApp: "employees.json"
 }
 
-module.exports = {environmentConst}
+module.exports = { environmentConst };
