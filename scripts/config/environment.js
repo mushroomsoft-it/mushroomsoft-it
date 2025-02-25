7b53838d985a5f642e3a447de7393d6355d351ec
Jamil Omar Falconi Aguirre <JamilOmar@users.noreply.github.com>
2025-02-24 20:11:16 -0500

    removed dotenv call for configuration

--- scripts/config/environment.js
+++ scripts/config/environment.js
@@ -1,9 +1,9 @@
-const dotenv = require('dotenv').config();
+//const dotenv = require('dotenv').config();
 
-if (dotenv.error) {
-  console.error('Error loading .env file');
-  process.exit(1);
-}
+//if (dotenv.error) {
+//  console.error('Error loading .env file');
+//  process.exit(1);
+//}
 
 const environmentConst = {
   powerAutomateUri: process.env.POWER_AUTOMATE_URL,
