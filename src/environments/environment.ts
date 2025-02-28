73916530ad44121f0eb1d3aa9951f90956ade933
Juan Pablo Cortez Mosquera <juanpacortexz93@gmail.com>
2025-02-27 19:17:25 -0500

    update: get in touch http uri added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   PUBLIC_ACCESS_KEY: '86b6ef66-80c5-4483-967b-655337afa9e0',
-  EMAIL_API: "https://prod-21.brazilsouth.logic.azure.com:443/workflows/3e7856b0b439461f9812c17c16df58c8/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=GYmWPPfNG0L5_pmg7mepG3FqAuu1C3IPT5fVTda6Mu4"
+  EMAIL_API: "https://prod-30.brazilsouth.logic.azure.com:443/workflows/5a3a649931c94b94a0fa978928cd18b3/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ewBcfSYOUO3vlHTUAkpuYVrwyCscr_4u6XLPeOHupUY"
 };
