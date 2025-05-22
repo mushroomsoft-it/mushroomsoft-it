f72fd0493608a63892f2a6e1b5cb94c752482428
Juan Pablo Cortez Mosquera <63519906+JuanpaCortez93@users.noreply.github.com>
2025-05-22 13:55:32 -0500

    feat(qa): dev, qa and prod environments added

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  STAGING: 'dev',
+  STAGING: 'qa',
   PUBLIC_ACCESS_KEY: '86b6ef66-80c5-4483-967b-655337afa9e0',
   EMAIL_API: 'https://api.web3forms.com/submit',
   BOT_FRAMEWORK_URL:
