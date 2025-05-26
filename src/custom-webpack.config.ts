ecba1976f3b8d473b4a5464a10134f3590a4fa4a
Juan Pablo Cortez Mosquera <63519906+JuanpaCortez93@users.noreply.github.com>
2025-05-26 13:49:52 -0500

    refact(notification.service.ts): qa env added, custon-webpack env file changed

--- src/custom-webpack.config.ts
+++ src/custom-webpack.config.ts
@@ -1,7 +1,12 @@
-import { EnvironmentPlugin } from 'webpack';
-
 const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack');
+
+const env = process.env['ENVIRONMENT'];
 
 module.exports = {
-  plugins: [new Dotenv()],
+  plugins: [
+    ...(env !== 'production' && env !== 'qa'
+      ? [new Dotenv()]
+      : [new webpack.EnvironmentPlugin(['COPILOT_URL_TOKEN'])]),
+  ],
 };
